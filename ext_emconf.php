f07e2bc8ad9b3a7b0c99e47a94f1359e845e7720
Oliver Wassenaar <oliver.wassenaar@wacon.de>
2025-03-11 16:35:57 +0100

    Update ext_emconf.php
    Description added

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Secrets',
-    'description' => '',
+    'description' => 'secrets is used to encrypt messages and share them as a one-time link. The extension is suitable for organizations that place particular emphasis on security and trustworthiness. Since all communication takes place via your own web server, no third-party tools from servers whose location is not known (DSGVO) are necessary. Your own domain name in the link is more trustworthy and makes a more professional impression.',
     'category' => 'plugin',
     'author' => 'Kerstin Schmitt',
     'author_email' => 'info@wacon.de',
